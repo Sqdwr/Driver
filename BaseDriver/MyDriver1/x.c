ffc7949bd0c1832f0f51a82eb6838a4bb886d6cf
Anduin <1020019998@qq.com>
2018-01-18 20:20:01 +0800

    First Commit

---
+++ BaseDriver/MyDriver1/x.c
@@ -0,0 +1,13 @@
+#include <ntddk.h>
+
+VOID Unload(PDRIVER_OBJECT DriverObject)
+{
+	KdPrint(("Unload Success!\n"));
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegString)
+{
+	KdPrint(("Entry Driver!\n"));
+	DriverObject->DriverUnload = Unload;
+	return STATUS_SUCCESS;
+}
